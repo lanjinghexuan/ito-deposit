955ad13f67a1464dedeaa13e179e1a5a8f620bba
lanjinghexuan <1258321826@qq.com>
2025-07-27 20:39:54 +0800

    文件上传

--- internal/server/http.go
+++ internal/server/http.go
@@ -2,22 +2,16 @@ package server
 
 import (
 	"context"
-	"fmt"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/middleware/selector"
 	"github.com/go-kratos/kratos/v2/transport/http"
-	kratosHttp "github.com/go-kratos/kratos/v2/transport/http"
 	jwtv5 "github.com/golang-jwt/jwt/v5"
-	minio1 "github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/credentials"
 	v1 "ito-deposit/api/helloworld/v1"
 	"ito-deposit/internal/conf"
 	"ito-deposit/internal/service"
-	"mime/multipart"
 	http2 "net/http"
-	"time"
 )
 
 func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, order *service.OrderService, user *service.UserService,
@@ -98,65 +92,4 @@ func corsFilter(next http2.Handler) http2.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
-}
-
-func downloadFile(ctx kratosHttp.Context) error {
-	req := ctx.Request() // 拿到 *http.Request
-	file, header, err := req.FormFile("file")
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	//url, err := UploadFile("aaa", file, *conf.Data)
-	var url string
-	// 接下来就能用 file、header.Filename、header.Size
-	fmt.Println("upload:", header.Filename, header.Size)
-
-	return ctx.Result(200, map[string]string{"url": url})
-}
-
-func UploadFile(objectName string, file *multipart.FileHeader, c *conf.Data) (string, error) {
-	//方法为实现参数自行编写
-	var addr string
-	addr = c.Minio.Endpoint
-	// 初始化 MinIO 客户端
-	minioClient, err := minio1.New(addr, &minio1.Options{
-		Creds:  credentials.NewStaticV4(c.Minio.AccessKeyId, c.Minio.AccessKeySecret, ""),
-		Secure: c.Minio.UseSsl,
-	})
-	if err != nil {
-		return "", fmt.Errorf("failed to initialize MinIO client: %v", err)
-	}
-
-	// 打开上传的文件
-	src, err := file.Open()
-	if err != nil {
-		return "", fmt.Errorf("failed to open uploaded file: %v", err)
-	}
-	defer src.Close()
-
-	// 获取文件信息
-	fileStat, err := file.Open()
-	if err != nil {
-		return "", fmt.Errorf("failed to get file stats: %v", err)
-	}
-	defer fileStat.Close()
-	objectName = fmt.Sprintf("%s/%s", time.Now().Format("2006-01-02"), objectName)
-	// 使用 PutObject 上传文件
-	_, err = minioClient.PutObject(
-		context.Background(),
-		c.Minio.BucketName,
-		objectName,
-		src,
-		file.Size,
-		minio1.PutObjectOptions{ContentType: file.Header.Get("Content-Type")},
-	)
-	if err != nil {
-		return "", fmt.Errorf("failed to upload file to MinIO: %v", err)
-	}
-
-	fmt.Printf("Successfully uploaded file %s to bucket %s as object %s\n",
-		file.Filename, c.Minio.BucketName, objectName)
-	return addr + "/" + c.Minio.BucketName + "/" + objectName, nil
 }
